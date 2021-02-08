0e025a33b8dcaec5005dc9a1ad828d294006a636
dianamajek7 <dianamajek.dd@gmail.com>
2021-02-08 00:13:50 +0000

    Added StockItem test and implementation

--- tests/SKU/StockItemTest.java
+++ tests/SKU/StockItemTest.java
@@ -2,8 +2,7 @@ package SKU;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StockItemTest {
 
@@ -19,5 +18,17 @@ public class StockItemTest {
         StockItem stockItem = new StockItem('A', 50);
         StockItem stockItem1 = new StockItem('B', 30);
         assertNotEquals(stockItem, stockItem1);
+    }
+
+    @Test
+    public void validateStockItem_Code() {
+        StockItem stockItem = new StockItem('A', 50);
+        assertEquals('A', stockItem.getItem());
+    }
+
+    @Test
+    public void validateStockItem_UnitPrice() {
+        StockItem stockItem = new StockItem('A', 50);
+        assertEquals(50, stockItem.getUnitPrice());
     }
 }
